6cc304a708cb65a625c092441546e3f5d33fdc21
chaofiber <ni.chao19970209@gmail.com>
2020-04-27 16:34:44 +0200

    combine

--- task2/2.2.py
+++ task2/2.2.py
@@ -7,9 +7,12 @@ from sklearn.metrics import mean_squared_error, roc_auc_score
 from sklearn import preprocessing
 import math
 import csv
+from xgboost import XGBRegressor
 from sklearn.feature_selection import SelectKBest, SelectFpr, SelectFdr, SelectFwe, f_classif, chi2, f_regression,SelectFromModel
 from sklearn.svm import SVR, SVC, LinearSVC
 from scipy import stats
+from statistics import mean 
+
 
 VITALS = ['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']
 TESTS = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total',
@@ -134,6 +137,14 @@ def feature_selectionfrommodel(data,y,num_feature):
 	#new_data = np.delete(new_data,idx,1)
     return new_data
 
+def feature_selection(data,y,num_feature):
+	select = SelectKBest(f_regression, k=num_feature).fit(data,y)
+	#select = SelectFromModel(estimator=Lasso(), threshold=-np.inf, max_features=num_feature).fit(data,y)
+	new_data = select.transform(data);
+	idx = select.get_support()
+	#print(idx)
+	#new_data = np.delete(new_data,idx,1)
+	return new_data
 	
 def feature_selection_by_corre(data,y):
 	for i in range(21):
@@ -154,10 +165,16 @@ def svc(X,y):
 	clf.fit(X,y);
 	return clf;
 
+def xgb(X,y):
+	xg_reg = XGBRegressor(objective ='binary:logistic', colsample_bytree = 0.3, learning_rate = 0.3,
+	max_depth = 5, alpha = 10, n_estimators = 100);
+	xg_reg.fit(X,y);
+	return xg_reg;
+
 def cross_validation(data, Y_train, kfold):
 	score = 0
 	score_train = 0
-	kfold = 2
+	kfold = 5
 	kf = KFold(n_splits = kfold)
 	alpha = 10
 	weight = 0
@@ -165,13 +182,14 @@ def cross_validation(data, Y_train, kfold):
 	for train_index, val_index in kf.split(data):
 		x_train, x_val= data[train_index], data[val_index]
 		y_train, y_val = Y_train[train_index], Y_train[val_index]
-		reg = svc(x_train, y_train)
-		y_val_pred = reg.predict_proba(x_val) # shape: (n_sample, n_class)
-		score += roc_auc_score(y_val, y_val_pred[:,1]) * len(y_val)
+		#reg = svc(x_train, y_train)
+		reg = xgb(x_train,y_train)
+		#y_val_pred = reg.predict_proba(x_val) # shape: (n_sample, n_class)
+		y_val_pred = reg.predict(x_val)
+		#score += roc_auc_score(y_val, y_val_pred[:,1]) * len(y_val)
+		score += roc_auc_score(y_val, y_val_pred) * len(y_val)
 		#score_train += (mean_squared_error(reg.predict(x_train), y_train)) * len(y_train)
 		#weight += reg.coef_
-	#print("{}-fold cross validation RMSE: {} with {} features".format(kfold, math.sqrt(score / len(Y_train)), n))
-	#print("{}-fold training RMSE: {} ".format(kfold, math.sqrt(score_train/ (len(Y_train)*kfold))))
 	return (score / len(Y_train))
 
 def print_to_csv(weight,idx):
@@ -188,12 +206,19 @@ def print_to_csv(weight,idx):
 	result.to_csv('./weight.csv',indata_processdex=False,header=False)
 	
 def do_task1(train, label_data, test):
+	total_score = [];
+	print("using 10 % of data");
 	for label in TESTS:
 		print(label)
 		x_data = train.sort_values('pid').values;
 		x_label = label_data.sort_values('pid')[label].values;
+		
+		# do feature selection before training
+		x_data = feature_selection(x_data,x_label,70);
 		score = cross_validation(x_data, x_label, 20);
+		total_score.append(score);
 		print("score of {}:{}".format(label, score));
+	print("average score of subtask1:{}".format(mean(total_score)));
 	return
 
 def do_task2(train, label_data, test):
@@ -209,15 +234,16 @@ def main():
 	test_path = './test_features.csv';
 	label_path = './train_labels.csv';
 	train, test, label = data_process(train_path, test_path, label_path); # still return pandaFrame
-	do_task1(train, label, test)
+	
     # if need values, just use 'train.values' it will return numpy array, label['LABEL_ABPm'].values to return labels.
-	# task 1do_task1()
+	# task 1
+	do_task1(train, label, test)
     # task 2	
 	train, test, label = data_processnorm(train_path, test_path, label_path); # still return pandaFrame
 	#new_train = feature_selectionKbest(train,label,num_feature)
 	new_train = feature_Univarselection(train,label,Alpha)
 	#new_train = feature_selectionfrommodel(train,label,num_feature)
-	do_task2(new_train, label, test)
+	#do_task2(new_train, label, test)
     # task 3
     
 	#kfold = 20
