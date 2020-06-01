6d1390aca34f7e8c4fcf1bec4ff68d07f81a7d8b
89ng <58104779+89ng@users.noreply.github.com>
2020-06-01 09:22:11 +0200

    Update 3.py

--- task3/3.py
+++ task3/3.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, Lasso, RidgeCV
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, Lasso, RidgeCV,SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.metrics import mean_squared_error, roc_auc_score, r2_score
+from sklearn.metrics import mean_squared_error, roc_auc_score, r2_score, f1_score
 from sklearn import preprocessing
 import math
 import csv
@@ -12,13 +12,69 @@ import random
 
 from sklearn.feature_selection import SelectKBest, SelectFpr, SelectFdr, SelectFwe, f_classif, chi2, f_regression,SelectFromModel
 from sklearn.svm import SVR, SVC, LinearSVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from scipy import stats
 from statistics import mean 
 
 
 num_feature = 60
 Alpha = 0.01
+Amino_acids = np.array(['R','H','K','D','E','S','T','N','Q','C','U','G','P','A','I','L','M','F','W','Y','V'])
 
+
+def encoder_sparse(train_path, test_path):
+    train = load_data(train_path)
+    test = load_data(test_path)
+    n_train,m_train = train.shape
+    n_test,m_test = test.shape
+    new_train = np.zeros(n_train*84).reshape(n_train,84)    
+    new_test = np.zeros(n_test*84).reshape(n_test,84)
+    Seq_train = train['Sequence'].values
+    Seq_test = test['Sequence'].values
+    label = train['Active'].values
+    for train_idx in range(n_train):
+        seq_train_arr = np.array(list(Seq_train[train_idx]))
+        for i in range(4):
+            seq_train_idx = int(np.where(Amino_acids == seq_train_arr[i])[0])
+            new_train[train_idx,i*21+seq_train_idx] = 1
+    
+    for test_idx in range(n_test):
+        seq_test_arr = np.array(list(Seq_test[test_idx]))
+        for j in range(4):
+            seq_test_idx = int(np.where(Amino_acids == seq_test_arr[j])[0])
+            new_test[test_idx,j*21+seq_test_idx] = 1
+            
+
+            
+    return new_train, new_test, label
+
+def encoder(train_path, test_path):
+    train = load_data(train_path)
+    test = load_data(test_path)
+    n_train,m_train = train.shape
+    
+    n_test,m_test = test.shape
+    new_train = np.zeros(n_train*4).reshape(n_train,4)    
+    new_test = np.zeros(n_test*4).reshape(n_test,4)
+    Seq_train = train['Sequence'].values
+    Seq_test = test['Sequence'].values
+    label = train['Active'].values
+    for train_idx in range(n_train):
+        seq_train_arr = np.array(list(Seq_train[train_idx]))
+        for i in range(4):
+            seq_train_idx = int(np.where(Amino_acids == seq_train_arr[i])[0])
+            new_train[train_idx,i] = seq_train_idx
+    
+    for test_idx in range(n_test):
+        seq_test_arr = np.array(list(Seq_test[test_idx]))
+        for j in range(4):
+            seq_test_idx = int(np.where(Amino_acids == seq_test_arr[j])[0])
+            new_test[test_idx,j] = seq_test_idx
+    
+    # if we should do normalization? if we set the nan to zero.
+
+    return new_train, new_test, label
 
 def data_process(train_path, test_path):
     train = load_data(train_path)
@@ -64,13 +120,14 @@ def data_process_mean(train_path,test_path,label_path):
 
 def load_data(data_path):
     data = pd.read_csv(data_path)
+    #data = data[0:10000]
     # X_train = train.drop(['Id','y'],axis=1)
     # y_train = train.y
     # X_test = test.drop(['Id'],axis=1)
     return data
     
 def regression(X,y):
-    xg_reg = XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.05,
+    xg_reg = XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.4,
 	max_depth = 5, alpha = 10, n_estimators = 160)
     xg_reg.fit(X,y)
     return xg_reg
@@ -184,14 +241,18 @@ def ridge(X,y,alpha):
 	return reg
 
 def svc(X,y):
-	clf = SVC(probability=True, class_weight='balanced');
-	clf.fit(X,y);
-	return clf;
+	#clf = LinearSVC(class_weight='balanced',max_iter = 10000);
+	#clf = SGDClassifier();
+    #clf = MLPClassifier(solver='sgd', alpha=1e-5,hidden_layer_sizes=(100,100), random_state=1,max_iter = 1000);   
+    #clf = LogisticRegression(class_weight='balanced',max_iter = 10000);
+    #clf = KNeighborsClassifier();
+    clf.fit(X,y);
+    return clf;
 
 
 def xgb(X,y):
-	xg_reg = XGBRegressor(objective ='binary:logistic', colsample_bytree = 0.3, learning_rate = 0.3,
-	max_depth = 5, alpha = 10, n_estimators = 100);
+	xg_reg = XGBRegressor(objective ='binary:hinge', colsample_bytree = 0.3, learning_rate = 0.05,
+	max_depth = 8, alpha = 10, n_estimators = 2500,scale_pos_weight = 50);
 	xg_reg.fit(X,y);
 	return xg_reg;
 
@@ -214,22 +275,21 @@ def cross_validation(data, Y_train, test):
     alpha = 10
     weight = 0
     m, n = data.shape
-    pred = []
     for train_index, val_index in kf.split(data):
         x_train, x_val = data[train_index], data[val_index]
         y_train, y_val = Y_train[train_index], Y_train[val_index]
-        # reg = svc(x_train, y_train)
-        reg = xgb(x_train, y_train)
+        #reg = svc(x_train, y_train)
+        reg = xgb(x_train, y_train)        
         #reg = logistic(x_train,y_train)
         # y_val_pred = reg.predict_proba(x_val) # shape: (n_sample, n_class)
         y_val_pred = reg.predict(x_val)
         # score += roc_auc_score(y_val, y_val_pred[:,1]) * len(y_val)
-        score += roc_auc_score(y_val, y_val_pred) * len(y_val)
+        score += f1_score(y_val, y_val_pred) * len(y_val)
         
-        pred.append(reg.predict(test));
+        pred = reg.predict(test);
     # score_train += (mean_squared_error(reg.predict(x_train), y_train)) * len(y_train)
     # weight += reg.coef_
-    return (score / len(Y_train)), np.mean(np.array(pred),0)
+    return (score / len(Y_train)), np.array(pred)
 
 def cross_validation_task2(data, Y_train, test):
 
@@ -291,33 +351,26 @@ def print_to_csv(weight, idx):
     result.to_csv('./weight.csv', indata_processdex=False, header=False)
 
 
-def do_task1(train, label_data, test):
-    total_score = [];
-    submit = pd.DataFrame()
-    test_id = test.sort_values('pid').index
-    submit['pid'] = test_id
-    for label in TESTS:
-        print(label)
-        x_data = train.sort_values('pid').values;
-        x_label = label_data.sort_values('pid')[label].values;
+def do_task(train, label_data, test):
 
-        # do feature selection before training
-        #x_data, test_selected = feature_selection(x_data, x_label, 70,test.sort_values('pid').values);
-        score, pred = cross_validation(x_data, x_label, test.sort_values('pid').values);
+    # do feature selection before training
+    #x_data, test_selected = feature_selection(x_data, x_label, 70,test.sort_values('pid').values);
+    score, pred = cross_validation(train, label_data, test);
 
-        submit[label] = pred;
-        
-        total_score.append(score);
-        print("score of {}:{}".format(label, score));
-    print("average score of subtask1:{}".format(mean(total_score)));
-    submit.to_csv('submission.csv',index=False)
+    print("f1_score :{}".format(score));
+    result = pd.DataFrame(pred)
+    result.to_csv('./sample.csv',index=False,header=False)
     return
 
 def main():
     train_path = './train.csv';
     test_path = './test.csv';
-    train, test, label = data_process(train_path, test_path);  # still return pandaFrame
-
+    train, test, label = encoder_sparse(train_path, test_path);  # still return pandaFrame
+    #train, test, label = encoder(train_path, test_path); 
+    print(train)
+    print(label)
+    do_task(train, label, test)
+    print("finish task3");
     # if need values, just use 'train.values' it will return numpy array, label['LABEL_ABPm'].values to return labels.
     
 
